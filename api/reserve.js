3c1f13cdc91ebf0d1d0ab2033c49d833a8910743
Rafi-Shah <rafishah7774440@gmail.com>
2026-06-18 16:48:11 +0600

    Switch to JS serverless function

--- api/reserve.js
+++ api/reserve.js
@@ -1,12 +1,12 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ message: "Only POST allowed" });
+    return res.status(405).json({
+      message: "Only POST allowed"
+    });
   }
-
-  const data = req.body;
 
   return res.status(200).json({
     success: true,
-    received: data
+    data: req.body
   });
 }
